211d5a2783fa8b2d96901de73f29552db00f45b6
Anderson <anderson@pop-os.localdomain>
2023-01-17 18:04:44 -0300

    Componente: Button

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@anderson-silva-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: { type: 'radio' },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+    size: {
+      options: ['sm', 'md'],
+      control: { type: 'inline-radio' },
+    },
+    children: {
+      control: { type: 'text' },
+    },
+  },
+} as Meta<ButtonProps>
+
+export const Basic: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'primary',
+  },
+}
+
+export const withIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next Step
+        <ArrowRight weight="bold" />
+      </>
+    ),
+  },
+}
